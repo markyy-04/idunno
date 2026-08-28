277825e3e6b310d7e77c0217037217feb650b505
markyy-04 <markderekdipasupil@gmail.com>
2026-08-29 03:20:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,148 +1,52 @@
-print("=============================")
-print("  Welcome Dear Pharmacist!") 
-print("=============================")
 
-print("\n------PHARMACIST LOGIN------")
+      import streamlit as st
 
-username = input("username: ")
-password = input("password  ")
+st.title("💊 PharmaKart")
+st.write("Welcome to Medicare Pharmacy!")
 
-if username == "mark" and password == "1234":
-    print ("Login Succesfull")
-    
-    order = []
-    
-    while True:
-        
-        print("\n============================")
-        print("        PharmaKart")
-        print("============================")
-        print("[1] Paracetamol")
-        print("[2] Vitamin C")
-        print("[3] Atorvastatin")
-        print("[4] Amlodipine")
-        print("[5] View Order")
-        print("[6] Exit")
-        
-        choice = input("Choose an option:")
-        
-        if choice == "1":
-            print("=======PARACETAMOL========")
-            print("[1] 325 mg - P4.50")
-            print("[2] 500 mg - P5.55")
-            
-            strength = input("Choose Strenght: ")
-            
-            if strength == "1":
-                medicine = "Paracetamol 325 mg"
-                price = 4.5
-                
-            elif strength == "2":
-                medicine = "Paracetamol 500 mg"
-                price = 5.55
-                
-            else:
-                print("Invalid choice.")
-                continue
-            
-            quantity = int(input("Enter quantity: "))
-            subtotal = price * quantity
+st.header("Pharmacist Login")
 
-            order.append([medicine, quantity, price, subtotal])
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
 
-            print("\nAdded to order!")
-            print("Medicine:", medicine)
-            print("Quantity:", quantity)
-            print("Subtotal: P", format(subtotal, ".2f"))
+if st.button("Login"):
 
-            print("sucessfully added to order!")
+    if username == "pharmacist" and password == "1234":
+        st.success("Login Successful!")
+        st.write("Welcome, Pharmacist", username)
 
-        elif choice == "2":
-            print("=======VITAMIN C=======")
-            print("[1] 500 mg -P2.25")
-            print("[2] 1000 mg - P7.50")
-            
-            strength = input("Choose Strenght: ")
-            
-            if strength == "1":
-                medicine = "Vitamin C 500 mg"
-                price = 2.25
-                
-            elif strength == "2":
-                medicine = "Vitamin C 1000 mg"
-                price = 7.50
-                
-            else:
-                print("Invalid choice.")
-                continue
-            quantity = int(input("Enter quantity: "))
-            subtotal = price * quantity
+        st.header("Main Menu")
 
-            order.append([medicine, quantity, price, subtotal])
+        choice = st.selectbox(
+            "Choose an option:",
+            [
+                "Select a medicine",
+                "Paracetamol",
+                "Vitamin C",
+                "Atorvastatin",
+                "Amlodipine"
+            ]
+        )
 
-            print("\nAdded to order!")
-            print("Medicine:", medicine)
-            print("Quantity:", quantity)
-            print("Subtotal: P", format(subtotal, ".2f"))
+        if choice == "Paracetamol":
+            st.subheader("Paracetamol")
+            st.write("325 mg - ₱4.50")
+            st.write("500 mg - ₱5.55")
 
-            print("sucessfully added to order!")
-            
-        elif choice == "3":
-            print("=======ATORVASTATIN=======")
-            print("[1] 20 mg -P17.25")
-            print("[2] 40 mg - P24.50")
-            
-            strength = input("Choose Strenght: ")
-            
-            if strength == "1":
-                medicine = "Atorvastatin 20 mg"
-                price = 17.25
-                
-            elif strength == "2":
-                medicine = "Atorvastatin 40 mg"
-                price = 24.50
-                
-            else:
-                print("Invalid choice.")
-                continue
-            quantity = int(input("Enter quantity: "))
-            subtotal = price * quantity
+        elif choice == "Vitamin C":
+            st.subheader("Vitamin C")
+            st.write("500 mg - ₱2.25")
+            st.write("1000 mg - ₱7.50")
 
-            order.append([medicine, quantity, price, subtotal])
+        elif choice == "Atorvastatin":
+            st.subheader("Atorvastatin")
+            st.write("20 mg - ₱17.25")
+            st.write("40 mg - ₱24.50")
 
-            print("\nAdded to order!")
-            print("Medicine:", medicine)
-            print("Quantity:", quantity)
-            print("Subtotal: P", format(subtotal, ".2f"))
+        elif choice == "Amlodipine":
+            st.subheader("Amlodipine")
+            st.write("5 mg - ₱6.25")
+            st.write("10 mg - ₱9.50")
 
-            print("sucessfully added to order!")
-        
-        elif choice == "4":
-            print("=======AMLODIPINE=======")
-            print("[1] 5 mg -P17.25")
-            print("[2] 10 mg - P24.50")
-            
-            strength = input("Choose Strenght: ")
-            
-            if strength == "1":
-                medicine = "Amlodipine 5 mg"
-                price = 6.45
-                
-            elif strength == "2":
-                medicine = "Amlodipine 10 mg"
-                price = 9.45
-                
-            else:
-                print("Invalid choice.")
-                continue
-            quantity = int(input("Enter quantity: "))
-            subtotal = price * quantity
-
-            order.append([medicine, quantity, price, subtotal])
-
-            print("\nAdded to order!")
-            print("Medicine:", medicine)
-            print("Quantity:", quantity)
-            print("Subtotal: P", format(subtotal, ".2f"))
-
-            print("sucessfully added to order!")
+    else:
+        st.error("Incorrect username or password.")
